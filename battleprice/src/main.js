2cbfd261a342f947eed3de508a7acf982fe11f4a
Pedro <pedrocascovardes@gmail.com>
2026-05-25 18:32:21 -0300

    Landing atualizada, logo, footer, header

--- battleprice/src/main.js
+++ battleprice/src/main.js
@@ -4,14 +4,12 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-<section id="center">
-  <div class="hero">
-    
-    
 
-<div class="ticks"></div>
-<section id="spacer"></section>
+document.querySelector('#app').innerHTML = `
+  <div class="tela-inicial">
+    <h1>BattlePrice</h1>
+    <p class="piscar">Clique em qualquer lugar para começar</p>
+  </div>
 `
 
 setupCounter(document.querySelector('#counter'))
